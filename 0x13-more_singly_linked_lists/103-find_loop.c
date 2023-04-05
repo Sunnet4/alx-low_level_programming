529632bc206964f09129165533f434691481fa78
Sunnet4 <awolaja.sunday@gmail.com>
2023-04-05 05:42:35 -0700

    up to date

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,30 +1,30 @@
 #include "lists.h"
 /**
  * find_listint_loop - finds the loop in a linked list
- * @head: linked list to search for
+ * @head: linked list to search for loop
  *
- * Return: address of the node where the loop starts, or NULL
+ * Return: address of the node where the loop begins, or NULL
  */
 listint_t *find_listint_loop(listint_t *head)
 {
- listint_t *slow = head;
- listint_t *fast = head;
- if (!head)
- return (NULL);
- while (slow && fast && fast->next)
- {
- fast = fast->next->next;
- slow = slow->next;
- if (fast == slow)
- {
- slow = head;
- while (slow != fast)
- {
- slow = slow->next;
- fast = fast->next;
- }
- return (fast);
- }
- }
- return (NULL);
+	listint_t *slow = head;
+	listint_t *fast = head;
+	if (!head)
+	return (NULL);
+	while (slow && fast && fast->next)
+	{
+	fast = fast->next->next;
+	slow = slow->next;
+	if (fast == slow)
+	{
+	slow = head;
+	while (slow != fast)
+	{
+	slow = slow->next;
+	fast = fast->next;
+	}
+	return (fast);
+	}
+	}
+return (NULL);
 }
