7c27c7fbcb1eab8ca7ab8dfb632112aac29a7547
Sunnet4 <awolaja.sunday@gmail.com>
2023-04-07 05:36:39 -0700

    binary to unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,26 +2,24 @@
 
 /**
  * function that converts a binary number to an unsigned int.
- * @b: where b is pointing to a string of 0 and 1 chars
+ * @b: is the binary string
  * Return: the converted number, or 0 if there is one or more chars in the string b
  * that is not 0 or 1 or if b is NULL.
  */
-
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int n = 0;
-	unsigned int dec = 0;
+	unsigned int j = 0, decimal = 0;
 
 	if (b == NULL)
 		return (0);
-	while (*(b + n) != '\0')
+	while (b[j] != '\0')
 	{
-		if (*(b + n) != '0' && *(b + n) != '1')
+		if (b[j] != '0' && b[j] != '1')
 			return (0);
-		dec <<= 1;
-		if (*(b + n) == '1')
-			dec ^= 1;
-		n++;
+		decimal <<= 1;
+		if (b[j] == '1')
+			decimal ^= 1;
+		j++;
 	}
-	return (dec);
+	return (decimal);
 }
