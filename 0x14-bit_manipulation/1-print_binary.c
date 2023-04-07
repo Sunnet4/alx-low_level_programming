5eb161667cc5c83d57f39573b5d248a4ca23035f
Sunnet4 <awolaja.sunday@gmail.com>
2023-04-07 06:56:16 -0700

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_binary : prints binary representation of a number
+ * print_binary - prints binary representation of a number
  * @n: input number to be converted to binary
  * Return: void
  */
